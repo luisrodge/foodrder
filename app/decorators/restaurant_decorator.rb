afa0f335cff149514fe571734adc191cc7c45aac
Luis Rodriguez <luisrodge@outlook.com>
2017-10-23 05:24:03 -0600

    Restaurant style update

--- app/decorators/restaurant_decorator.rb
+++ app/decorators/restaurant_decorator.rb
@@ -6,7 +6,7 @@ class RestaurantDecorator < Draper::Decorator
   end
 
   def menu_food_counter
-    h.pluralize(object.menus.count, "Menu") + ', ' + h.pluralize(object.foods.count, "Total Food")
+    h.pluralize(object.menus.count, "Menu") + ', ' + h.pluralize(object.foods.count, "Food") + " In Total"
   end
 
 end
