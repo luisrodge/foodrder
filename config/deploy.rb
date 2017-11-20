d681a276b616e29f19da262e9de9c6b431168284
Luis Rodriguez <luisrodge@outlook.com>
2017-11-19 23:07:34 -0600

    Add seed task to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,3 +45,16 @@ set :deploy_to, '/home/deploy/foodrder'
 
 append :linked_files, "config/database.yml", "config/secrets.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
+
+
+# Seed production database
+desc 'Runs rake db:seed'
+task :seed => [:set_rails_env] do
+  on primary fetch(:migration_role) do
+    within release_path do
+      with rails_env: fetch(:rails_env) do
+        execute :rake, "db:seed"
+      end
+    end
+  end
+end
