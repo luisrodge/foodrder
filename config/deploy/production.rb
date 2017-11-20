1980d2e8c09ff640c314c101776faaac51934dec
Luis Rodriguez <luisrodge@outlook.com>
2017-11-19 18:55:07 -0600

    Edit production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,3 +59,6 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+
+# Replace 127.0.0.1 with your server's IP address!
+server '104.236.67.95', user: 'deploy', roles: %w{app db web}
