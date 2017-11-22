e06ddd3ad677a4746ef05f60e546f78ff121c3d6
Luis Rodriguez <luisrodge@outlook.com>
2017-11-22 13:08:52 -0600

    update foods controller search query

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,5 @@
 class FoodsController < ApplicationController
   def index
-    @foods = params[:mc].present? ? Food.search(params[:mc], fields: [:tags_name]) : Food.order("created_at DESC")
+    @foods = params[:mc].present? ? Food.search(params[:mc]) : Food.order("created_at DESC")
   end
 end
