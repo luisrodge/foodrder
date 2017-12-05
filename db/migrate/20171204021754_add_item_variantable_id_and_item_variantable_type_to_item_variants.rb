ad1f2f617e1eee96ebfb1a27f93a80eeb051a65f
Luis Rodriguez <luisrodge@outlook.com>
2017-12-04 22:02:30 -0600

    Fix ItemVariant migration error

--- db/migrate/20171204021754_add_item_variantable_id_and_item_variantable_type_to_item_variants.rb
+++
@@ -1,7 +0,0 @@
-class AddItemVariantableIdAndItemVariantableTypeToItemVariants < ActiveRecord::Migration[5.1]
-  def change
-    add_column :item_variants, :item_variantable_id, :integer, index: true
-    add_column :item_variants, :item_variantable_type, :string, index: true
-
-  end
-end
--- db/migrate/20171204023158_drop_item_variants.rb
+++
@@ -1,5 +0,0 @@
-class DropItemVariants < ActiveRecord::Migration[5.1]
-  def change
-    drop_table :item_variants
-  end
-end
