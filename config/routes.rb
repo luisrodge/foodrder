ec60c8e6001e52db73e37ee9b2ebf52ceaa49cb1
Luis Rodriguez <luisrodge@outlook.com>
2017-12-20 08:09:28 -0600

    Readd devise registrations routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   # Custom registration routes for customer and admins
-  # devise_for :customer, controllers: {registrations: 'customer/registrations'}, skip: :sessions
-  # devise_for :sellers, controllers: {registrations: 'seller/registrations'}, skip: :sessions
-  # devise_for :admins, controllers: {registrations: 'admin/registrations'}, skip: :sessions
+  devise_for :customer, controllers: {registrations: 'customer/registrations'}, skip: :sessions
+  devise_for :sellers, controllers: {registrations: 'seller/registrations'}, skip: :sessions
+  devise_for :admins, controllers: {registrations: 'admin/registrations'}, skip: :sessions
 
   # Single sign in route for customer, sellers, and admins
   devise_for :users, controllers: {sessions: 'sessions'}, skip: %i[sessions registrations]
