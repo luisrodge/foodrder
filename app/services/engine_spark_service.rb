28dcee02d05e5ac3d27c233c303a73eca13892fd
Luis Rodriguez <luisrodge@outlook.com>
2018-01-05 12:17:05 -0600

    Add reservation sms job and sms message

--- app/services/engine_spark_service.rb
+++ app/services/engine_spark_service.rb
@@ -3,22 +3,29 @@ require 'httparty'
 class EngineSparkService
   API_URL = 'https://start.engagespark.com/api/v1/engagements/287096/contacts/'
 
-  def initialize(order_fragment)
-    @order_fragment = order_fragment
+  def initialize(record)
+    @record = record
   end
 
   # Build out the new order text messages that
   # will be dispatched to the restaurant.
   def message_restaurant
-    dispatch_message("New order placed on foodrder.bz. View here foodrder.bz/seller/order_fragments/#{@order_fragment.id}",
-                     @order_fragment.restaurant.phone_number)
+    dispatch_message("New order placed on foodrder.bz. View here foodrder.bz/seller/order_fragments/#{@record.id}",
+                     @record.restaurant.phone_number)
+  end
+
+  # Build out message to inform restaurant
+  # about new reservation request.
+  def message_restaurant_reservation
+    dispatch_message("New reservation placed on foodrder.bz. View here foodrder.bz/seller/reservations",
+                     @record.restaurant.phone_number)
   end
 
   # Build out message to inform the customer
   # that their placed order is ready for pickup.
   def message_customer
-    dispatch_message("Your recently placed order at #{@order_fragment.restaurant.name} restaurant is ready for pickup.",
-                     @order_fragment.order.phone_number)
+    dispatch_message("Your recently placed order at #{@record.restaurant.name} restaurant is ready for pickup.",
+                     @record.order.phone_number)
   end
 
   def dispatch_message(message, phone_number)
