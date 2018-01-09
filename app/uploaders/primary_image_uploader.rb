1c2b6f73c664d53a6858552896002505265dd542
Luis Rodriguez <luisrodge@outlook.com>
2018-01-09 16:40:39 -0600

    Update primary image uploader

--- app/uploaders/primary_image_uploader.rb
+++ app/uploaders/primary_image_uploader.rb
@@ -10,7 +10,7 @@ class PrimaryImageUploader < CarrierWave::Uploader::Base
   version :medium do
     eager
     process resize_to_fill: [400, 320]
-    cloudinary_transformation :quality => 100
+    cloudinary_transformation :quality => 80
   end
 
   version :thumb do
